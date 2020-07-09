0aa2a2c552a07b62120bb1461077b6d2b77b3167
Hardy <1277146050@qq.com>
2020-07-09 14:30:25 +0800

    add files

---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <div id="cover"></div>
+      <Header></Header>
+      <Footer></Footer>
+      <Todo></Todo>    
+  </div>
+</template>
+
+<script>
+import Header from './todo/header.vue'
+import Footer from './todo/footer.jsx'
+import Todo from './todo/todo.vue'
+export default {
+     components:{
+         Header,
+         Footer,
+         Todo
+     }  
+}
+</script>
+
+<style>
+  
+</style>
